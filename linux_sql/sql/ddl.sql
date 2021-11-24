5d55ca546bdd97402483f0ff1c8329e70553821f
Adrian Zaia <ediezaia@gmail.com>
2021-11-24 19:06:19 +0000

    fixed ddl

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -1,1 +1,21 @@
+CREATE TABLE PUBLIC.host_info (
+    id SERIAL   	PRIMARY KEY NOT NULL,
+    host_name   	VARCHAR NOT NULL,
+    cpu_number  	INTEGER NOT NULL,
+    cpu_architecture  	VARCHAR NOT NULL,
+    cpu_model   	VARCHAR NOT NULL,
+    cpu_mhz     	FLOAT(3) NOT NULL,
+    12_cache    	INT NOT NULL,
+    total_mem   	INT NOT NULL,
+    'timestamp' 	TIMESTAMP NOT NULL
+    );
 
+CREATE TABLE PUBLIC.host_usage (
+    "timestamp" 	TIMESTAMP NOT NULL,
+     host_id 		SERIAL NOT NULL,
+     memory_free 	INT NOT NULL,
+     cpu_idle 		INT NOT NULL,
+     cpu_kernel 	INT NOT NULL,
+     disk_io 		INT NOT NULL,
+     disk_available 	INT NOT NULL
+     );
