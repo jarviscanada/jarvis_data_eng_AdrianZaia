570b38f9f23a8076c8ea6ecfa45c17398f9e3b48
Adrian Zaia <ediezaia@gmail.com>
2021-11-25 18:32:01 +0000

    updated host_usage.sh

---
+++ linux_sql/scripts/host_usage.sh
@@ -0,0 +1,40 @@
+#! /bin/sh
+
+#Setup Arguments
+psql_host=$1
+psql_port=$2
+dl_name=$3
+psql_user=$4
+psql_password=$5
+
+#Check For Valid Number of Arguments
+if [ $# -ne 5]; then
+  echo "Please Input Five (5) Arguments"
+  exit 1
+fi
+
+#Retrieve Specifications
+vmstat_mb=$(vmstat --unit M)
+hostname=$(hostname -f)
+
+memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
+cpu_idle=$(echo "$vmstat_mb"| awk '{print $5}'|tail -n1| xargs)
+cpu_kernal=$(echo "vmstat_mb"| awk '{print $6}'| tail -n1| xargs)
+disk_io=$(vmstat -d| awk '{print $10}'|tail -n1| xargs)
+disk_available=$(df -BM| awk '{print 4}' | tail -n1| xargs)
+
+#Find Current Time
+timestamp=$(date '+%F %T')
+
+#Query For Matching ID through host_info Table
+host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
+
+#Insert Info Into Database
+insert_stmt="INSERT INTO host_usage VALUES('$timestamp', $host_id, $memory_free, $cpu_idle, $cpu_kernal, $disk_io, $disk_available)"
+
+#Exporting Password
+export PGPASSWORD=$psql_psql_password
+
+#Insert Data into Database (Using PSQL, CLI Argument and Insert Statement)
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "insert_stmt"
+exit 0
