be00ec029c01a5568c4696a4d108ddb3577754f4
Adrian Zaia <46577552+EdZaia@users.noreply.github.com>
2022-01-09 15:16:58 -0500

    Update JavaGrepImp

--- core_java/grep/src/JavaGrepImp.java
+++ core_java/grep/src/JavaGrepImp.java
@@ -1,14 +1,70 @@
 package ca.jrvs.apps.grep;
 
 import java.io.*;
+import java.util.*;
+import org.slf4j.*;
+import org.apache.*;
 
 public class JavaGrepImp implements JavaGrep{
 
     final Logger logger = LoggerFactory.getLogger(JavaGrep.class);
 
+    //Private Variables
     private String regex;
     private String rootPath;
     private String outFile;
+
+    //regex variable
+    public String getRegex() {
+        return regex;
+    }
+    public void setRegex(String regex){
+        this.regex = regex;
+    }
+
+
+    //rootPath variable
+    public String getRoothPath() {
+        return rootPath;
+    }
+    public void setRoothPath(String rootPath){
+        this.rootPath = rootPath;
+    }
+
+
+    //outFile variable
+    public String getOutFile() {
+        return outFile;
+    }
+    public void setOutFile(String outFile){
+        this.outFile = outFile;
+    }
+
+
+    public List<File> listFiles(String rootPath){
+        File root = new File(rootPath);
+        return Arrays.asList(root.listFiles());
+    }
+    public List<String> readLines(String rootPath){
+        Path filePath = inputFile.toPath();
+        return Files.readAllLines(filePath);
+    }
+
+    public boolean containsPattern(String input){
+        return input.matches(regex);
+    }
+
+    public void writeToFile(List<String> output) throws IOException {
+        FileWriter write = new FileWriter(outFile);
+        for (String input:lines) {
+            writer.write(input + System.lineSeparator());
+        }
+        writer.close();
+    }
+
+
+
+
 
     public static void main(String[] args){
         if(args.legeth !=3){
