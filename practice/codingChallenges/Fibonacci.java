61f322c40f6f4e49968bc43ff3616227e47bb04c
Adrian Zaia <46577552+EdZaia@users.noreply.github.com>
2022-02-23 11:06:21 -0500

    Created Fibonacci.java

---
+++ practice/codingChallenges/Fibonacci.java
@@ -0,0 +1,16 @@
+class Fibonacci {
+    public int fib(int n) {
+        // list to store numbers calculate so far
+        int f[] = new int[n+2];
+        
+        // the first two numbers are 0 and 1
+        f[0] = 0;
+        f[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            //add the previous 2 numbers and store it
+            f[i] = f[i-1] + f[i-2];
+        }
+        return f[n];
+    }
+}
