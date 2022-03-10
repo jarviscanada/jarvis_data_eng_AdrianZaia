eb8989079e9b95f8cea1aa27b49294c1a8458536
Adrian Zaia <46577552+EdZaia@users.noreply.github.com>
2022-03-10 16:23:25 -0500

    Updated RemoveElement.java

--- practice/codingChallenges/RemoveElement.java
+++ practice/codingChallenges/RemoveElement.java
@@ -1,3 +1,10 @@
+/*
+time complexity: O(n)
+solved using two pointers (i and j)
+i is a slow runner that tracks the final number of elements
+j is a fast runner that we use to iterate over the list
+*/
+
 class RemoveElement {
     public int removeElement(int[] nums, int val) {
         int i = 0;
