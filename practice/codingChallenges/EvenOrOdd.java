cb67998ecc7b144a826aa880447daed507b3a334
Adrian Zaia <46577552+EdZaia@users.noreply.github.com>
2022-03-10 16:23:11 -0500

    Update EvenOrOdd.java

--- practice/codingChallenges/EvenOrOdd.java
+++ practice/codingChallenges/EvenOrOdd.java
@@ -5,21 +5,21 @@ import java.io.InputStreamReader;
 public class EvenOrOdd{
   public static void main(String[] args) throws IOException{
     try{
-            int n;
-            BufferedReader in = 
-            new BufferedReader(new InputStreamReader(System.in));
-            n = Integer.parseInt(in.readLine());
+          int n;
+          BufferedReader in = 
+          new BufferedReader(new InputStreamReader(System.in));
+          n = Integer.parseInt(in.readLine());
         if (n % 2 == 0)
         {
-            System.out.println("The number is Even.");
+          System.out.println("The number is Even.");
         }
         else
         {
-            System.out.println("The number is Odd.");
+          System.out.println("The number is Odd.");
         }
     } catch(NumberFormatException e){
-         System.out.println(e.getMessage() + " is not a numeric value.");
-         System.exit(0);
+       System.out.println(e.getMessage() + " is not a numeric value.");
+       System.exit(0);
         }
   }
 } 
